be8bfc834c7d16bac084d37fdc09f37d896e0480
KARTHIKEYAN <125242044+askarthi1611@users.noreply.github.com>
2026-03-14 21:43:39 +0530

    14-03-2026

--- dist/share/server/main.server.mjs
+++ dist/share/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-W5BV5VOP.mjs";import"./chunk-VWSEJEK3.mjs";import"./chunk-7EIYGGA5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7Z7OVRXI.mjs";import"./chunk-VWSEJEK3.mjs";import"./chunk-7EIYGGA5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://asks99.onrender.com/api',
-  // localApi:'http://localhost:3000/api',
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'https://asks99.onrender.com/api',
+  localApi:'http://localhost:3000/api',
+  // apiBaseUrl: 'http://localhost:3000/api',
 };
